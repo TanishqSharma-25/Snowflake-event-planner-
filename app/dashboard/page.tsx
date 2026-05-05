44b54141388f3d068a3a8aa8a4cc1ee552786893
TanishqSharma-25 <tanishqsharma2504@gmail.com>
2026-05-05 22:11:17 +0530

    Fix null session error

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,12 @@
 import { DashboardContent } from "@/components/dashboard-content";
 import { getSession } from "@/lib/auth/server";
+import { redirect } from "next/navigation";
 
 
 export default async function DashboardPage(){
     const session = await getSession()
+    if (!session?.data?.user) {
+    redirect("/login"); // or wherever your auth page is
+  }
     return <DashboardContent  userId={session.data.user.id} />
 }
